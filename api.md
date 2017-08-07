3c685c7d32cd29eb14b85479e0cdf904912afc2c
handora <qcdsr970209@gmail.com>
2017-08-07 11:35:37 +0800

    add api documents

---
+++ api.md
@@ -0,0 +1,116 @@
+# API文档
+#写得比较简陋不要在意
+#你可以先测试起来<br>
+
+**Get Articles(20/per request) :**
+```javascript
+url: '../api/v1.0/articles?page=(page)'
+method: GET
+args: 
+	page // the articles between (page-1)*20 
+        // to page*20 will be returned to you
+        
+response: 
+{
+	Code //200 is ok, else is error
+    Text //if error, there will be some error message
+    Body //an array of Artcile struct
+    	 // 我没有给count, 想要的话跟我说
+}
+
+Article struct:
+{
+	id      int        // sql id，可能无用
+	title   string     // 标题
+	content string     //文章内容
+	tag     []string   // tags of article, may be one
+	time    time.Time  //yyyy-mm-dd hh:mm:ss
+    // 类似sql里面的datetime数据类型。
+}
+```
+<br>
+<br>
+
+**Get Article() :**
+```javascript
+{
+    url: '../api/v1.0/articles/(id)'
+    method: GET
+    args: 
+        id // the article sql id
+
+    response: 
+    {
+        Code //200 is ok, else is error
+        Text //if error, there will be some error message
+        Body //a Artcile struct
+    }
+
+    Article struct:
+    {
+        id      int        // sql id，可能无用
+        title   string     // 标题
+        content string     //文章内容
+        tag     []string   // tags of article, may be one
+        time    time.Time  //yyyy-mm-dd hh:mm:ss
+        // 类似sql里面的datetime数据类型。
+    }
+}
+```
+<br>
+**Get Tags:**
+```javascript
+{
+	url: '../api/v1.0/tags'
+	method: GET
+	args: 
+		page // the articles between (page-1)*20 
+        	 // to page*20 will be returned to you
+      
+        
+    response: 
+    {
+        Code //200 is ok, else is error
+        Text //if error, there will be some error message
+        Body //an array of Article struct
+    }
+ 	
+    Article struct:
+    {
+        id      int        // sql id，可能无用
+        title   string     // 标题
+        content string     //文章内容
+        tag     []string   // tags of article, may be one
+        time    time.Time  //yyyy-mm-dd hh:mm:ss
+        // 类似sql里面的datetime数据类型。
+    }
+    
+}
+```
+
+<br>
+**Get Articles through Tag id:**
+```javascript
+{
+	url: '../api/v1.0/tags/(id)/articles?page=(page)'
+	method: GET
+	args: 
+	
+        
+    response: 
+    {
+        Code //200 is ok, else is error
+        Text //if error, there will be some error message
+        Body //an array of Tag struct
+    }
+
+    Tag struct:
+    {
+        id      int        // sql id，通过id找文章
+        name    string     // tags' name
+    }
+}
+```
+<br/>
+备注:
+POST方面如果你想写个管理界面，我就把api给你，如果你不想的写的话，就通过命令行管理，我来写好了.我哪里没写跟我说，如果有改动我也会跟你说的，我写了大致一半，休息2天在写<br>~(>-<)~
--- api/v1.0/articles/articles.go
+++ api/v1.0/articles/articles.go
@@ -1,4 +1,4 @@
-package article
+package articles
 
 import (
 	"blog/util"
@@ -29,7 +29,18 @@ type RequestBody struct {
 	tag     []string
 }
 
-func GetArticles(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+type jsonOk struct {
+	Code int
+	Text string
+}
+
+type getOk struct {
+	Code int
+	Text string
+	Body interface{}
+}
+
+func GetArticles(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
 	// get the page argument for pagination
 	page := int(ps.ByName("page"))
 
@@ -68,12 +79,12 @@ func GetArticles(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	defer rows.Close()
 
-	if err := json.NewEncoder(w).Encode(articles); err != nil {
+	if err := json.NewEncoder(w).Encode(getOk{Code:http.StatusOK, Text: "Get articles ok", body: articles}); err != nil {
 		panic(err)
 	}
 }
 
-func GetArticle(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func GetArticle(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
 	// get the page argument for pagination
 	id := int(ps.ByName("id"))
 
@@ -112,7 +123,7 @@ func GetArticle(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	if ok {
 		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(article); err != nil {
+		if err := json.NewEncoder(w).Encode(getOk{Code: http.StatusOK, Text: "Get article ok", Body: article}); err != nil {
 			panic(err)
 		}
 	} else {
@@ -128,48 +139,40 @@ func PostArticle(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	rb := RequestBody{}
 	json.NewDecoder(r.Body).Decode(&rb)
 
+	stmt, err := util.Db.Prepare("insert Article SET title=?,content=?,time=NOW()")
+	util.CheckError(err)
+
+	res, err := stmt.Exec(rb.title, rb.content)
+	util.CheckError(err)
+
+	id, err := res.LastInsertId()
+	util.CheckError(err)
+
+	for i := range rb.tag {
+		stmt, err = util.Db.Prepare("select name from Tag where id=?")
+		util.CheckError(err)
+
+		row := stmt.QueryRow(i)
+
+		var tmp string
+		err := row.Scan(&tmp)
+		util.CheckError(err)
+
+		stmt, err = util.Db.Prepare("INSERT ATrelation SET aid=?,tid=?")
+		util.CheckError(err)
+
+		res, err = stmt.Exec(id, i)
+		util.CheckError(err)
+	}
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-	var article Article
-
-	stmt, err := util.Db.Prepare("select Tag.name from Tag, ATrelation " +
-		"where ATrelation.aid=? and ATrelation.tid=Tag.id")
-	util.CheckError(err)
-
-	tags, err := stmt.Query(id)
-	util.CheckError(err)
-
-	for tags.Next() {
-		var tag string
-		tags.Scan(&tag)
-		article.tag = append(article.tag, tag)
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(jsonOk{Code: http.StatusOK, Text: "POST SUCCESSFULLY"}); err != nil {
+		panic(err)
 	}
+}
 
-	tags.Close()
+func UpdateArticle(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
-	stmt, err = util.Db.Prepare("select * from Article where Article.id=?")
-	util.CheckError(err)
-
-	rows, err := stmt.Query(id)
-	util.CheckError(err)
-
-	ok := false
-
-	for rows.Next {
-		ok = true
-		rows.Scan(&article.id, &article.title, &article.content, &article.time)
-	}
-	defer rows.Close()
-
-	if ok {
-		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(article); err != nil {
-			panic(err)
-		}
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusNotFound, Text: "Not Found"}); err != nil {
-			panic(err)
-		}
-	}
 }
