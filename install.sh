322a8e3c57acab67afb83a2e9b2c2ad2c813bd74
Handora <qcdsr970209@gmail.com>
2017-08-09 21:11:30 +0800

    purify

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 go get github.com/go-sql-driver/mysql
+echo "install go mysql OK\n"
 go get github.com/julienschmidt/httprouter
-go get github.com/julienschmidt/httprouter
+echo "install go httprouter OK\n"
 
 go install blog
+echo "install go blog OK\n"
